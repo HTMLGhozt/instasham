a05befb7f97fc8a07adde2d8792b8ffc0cd8d5bc
Thomas Dillard <thomas.dillard.studios@gmail.com>
2019-01-18 01:43:21 -0700

    minor changes

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import { HotModuleReplacementPlugin } from 'webpack';
+import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const BUILD = path.resolve(__dirname, './dist');
@@ -51,10 +51,41 @@ export default {
     ],
   },
   plugins: [
-    new HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       title: 'testing',
       template: 'public/index.html',
     }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   mangle: true,
+    //   unused: true,
+    //   dead_code: true, // big one--strip code that will never execute
+    //   warnings: false, // good for prod apps so users can't peek behind curtain
+    //   drop_debugger: true,
+    //   conditionals: true,
+    //   evaluate: true,
+    //   drop_console: true, // strips console statements
+    //   sequences: true,
+    //   booleans: true,
+    //   compress: {
+    //     warnings: false, // Suppress uglification warnings
+    //     pure_getters: true,
+    //     unsafe: true,
+    //     unsafe_comps: true,
+    //     screw_ie8: true
+    //   },
+    //   output: {
+    //     comments: false,
+    //   },
+    // }),
+    // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'vendor',
+    //   minChunks: function (module) {
+    //       // this assumes your vendor imports exist in the node_modules directory
+    //   // and will move all modules from there to separate bundle
+    //       return module.context && module.context.indexOf('node_modules') !== -1;
+    //   }
+    // }),
   ],
 };
